57149a404989fc44a012628f4506b7c556d4b36d
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2016-12-07 22:25:12 +0100

    Krok 9: wsparcie dla history.pushState

--- demo-app/src/main/java/net/lipecki/demo/DemoApplication.java
+++ demo-app/src/main/java/net/lipecki/demo/DemoApplication.java
@@ -2,11 +2,24 @@ package net.lipecki.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Bean
+	public ErrorViewResolver customErrorViewResolver() {
+		final ModelAndView redirectToIndexHtml = new ModelAndView("forward:/index.html", Collections.emptyMap(), HttpStatus.OK);
+		return (request, status, model) -> status == HttpStatus.NOT_FOUND ? redirectToIndexHtml : null;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }
